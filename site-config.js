13729fe09df46b3ce0cfa8b1f70eea6f408ab112
Parth <dagaduparth@hmai.com>
2026-02-27 02:09:15 +0530

    Use extensionless thank-you route for redirect

--- site-config.js
+++ site-config.js
@@ -3,5 +3,5 @@
   // Example: "https://formspree.io/f/xxxxxx"
   leadWebhookUrl: "https://formspree.io/f/mlgwzvoj",
   leadWebhookMethod: "POST",
-  thankYouPath: "/thank-you.html"
+  thankYouPath: "/thank-you"
 };
